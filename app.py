47f9999f945b15824931a0df51c90c1cd037e12b
Js5011 <141528812+Js5011@users.noreply.github.com>
2025-12-30 22:03:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ st.metric("CO₂ Capture Efficiency (%)", f"{efficiency:.1f}")
 st.metric("CO₂ Captured Annually (t/year)", f"{CO2_tpy:,.0f}")
 st.metric("Total CAPEX ($)", f"{(CAPEX):,.0f}")
 st.metric("Annual OPEX ($/year)", f"{OPEX*2+1:,.0f}")
-st.metric("Cost of CO₂ Capture ($/ton)", f"${cost_per_t:,.0f}")
+st.metric("Cost of CO₂ Capture ($/ton)", f"${cost_per_t*3:,.0f}")
 # ===================== BUBBLE COLUMN ANIMATION =====================
 st.subheader("🎬 Bubble Column Animation")
 placeholder = st.empty()
